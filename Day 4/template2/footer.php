cc33b5a336d741030e421fefd077efa06974ecc0
Sakina277 <83978988+Sakina277@users.noreply.github.com>
2021-05-31 18:38:29 +0530

    day 4

---
+++ Day 4/template2/footer.php
@@ -0,0 +1,4 @@
+
+<footer class="w3-center w3-black w3-padding-16">
+  <p>All @Copyrights are reserved 2021</p>
+</footer>
---
+++ Day 4/template2/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<title>Template 2</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>Learn </b> with us</a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#home" class="w3-bar-item w3-button">Home</a>
+      <a href="about.php" class="w3-bar-item w3-button">About</a>
+      <a href="courses.php" class="w3-bar-item w3-button">Courses</a>
+      <a href="contact.php" class="w3-bar-item w3-button">Contact</a>
+    </div>
+  </div>
+</div>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="img1.jpg" alt="test" width="1500" height="800">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>Learn</b></span> <span class="w3-hide-small w3-text-light-grey">With Us</span></h1>
+  </div>
+</header>
+
+
+
+  
+
+
+
+</body>
+</html>
